98ac5a9b564f3bc5abb0a8e2dcb0e1ff773e1df9
Asif Mohiuddin <mohiuddin.asif@gmail.com>
2021-12-02 18:59:10 +0100

    facebook verification added

--- packages/twentytwenty-theme/src/components/index.js
+++ packages/twentytwenty-theme/src/components/index.js
@@ -34,7 +34,7 @@ const Theme = ({ state }) => {
         <html lang="bn" />
         <meta name="robots" content="nofollow, noindex" />
         <link rel="canonical" href="https://www.shongshoy.com"></link>
-               
+        <meta name="facebook-domain-verification" content="2hqv17nofw0wuronu3mtxgzohw5fqb" />  
       </Head>
 
 
