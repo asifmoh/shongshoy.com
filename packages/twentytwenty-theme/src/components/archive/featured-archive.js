5d5bb095d3aa123a98d472384edffb7bb4adb214
Asif Mohiuddin <mohiuddin.asif@gmail.com>
2021-12-03 21:05:35 +0100

    source url change 2

--- packages/twentytwenty-theme/src/components/archive/featured-archive.js
+++ packages/twentytwenty-theme/src/components/archive/featured-archive.js
@@ -15,7 +15,7 @@ export default class featuredArchive extends Component {
                 <span className="slide__image">
                   <img
                     alt="নারীর সর্বোচ্চ সম্মান এবং সুমহান মর্যাদা!"
-                    src="https://www.nastikya.com/wp-content/uploads/2021/01/feminism.png"
+                    src="https://i2.wp.com/www.shongshoy.com/wp-content/uploads/2021/01/feminism.png"
                   />
                   <div className="text-block">
                     <a href="/%e0%a6%b8%e0%a7%81%e0%a6%ae%e0%a6%b9%e0%a6%be%e0%a6%a8-%e0%a6%ae%e0%a6%b0%e0%a7%8d%e0%a6%af%e0%a6%be%e0%a6%a6%e0%a6%be-%e0%a6%8f%e0%a6%ac%e0%a6%82-%e0%a6%b8%e0%a6%ae%e0%a6%be%e0%a6%a8-%e0%a6%85/">
@@ -29,7 +29,7 @@ export default class featuredArchive extends Component {
                 <span className="slide__image">
                   <img
                     alt="নবী মুহাম্মদের করুণ মৃত্যু"
-                    src="https://www.nastikya.com/wp-content/uploads/2020/06/death_shongshoy.jpg"
+                    src="https://i2.wp.com/www.shongshoy.com/wp-content/uploads/2020/06/death_shongshoy.jpg"
                   />
                   <div className="text-block">
                     <a href="/%e0%a6%a8%e0%a6%ac%e0%a7%80-%e0%a6%ae%e0%a7%81%e0%a6%b9%e0%a6%be%e0%a6%ae%e0%a7%8d%e0%a6%ae%e0%a6%a6%e0%a7%87%e0%a6%b0-%e0%a6%95%e0%a6%b0%e0%a7%81%e0%a6%a8-%e0%a6%ae%e0%a7%83%e0%a6%a4%e0%a7%8d/">
@@ -43,7 +43,7 @@ export default class featuredArchive extends Component {
                 <span className="slide__image">
                   <img
                     alt="কোরআন সংকলন এবং পরিমার্জনের ইতিহাস"
-                    src="https://www.nastikya.com/wp-content/uploads/2021/01/editing_Quran.jpg"
+                    src="https://i0.wp.com/www.shongshoy.com/wp-content/uploads/2021/01/editing_Quran.jpg"
                   />
                   <div className="text-block">
                     <a href="/%e0%a6%95%e0%a7%8b%e0%a6%b0%e0%a6%86%e0%a6%a8-%e0%a6%b8%e0%a6%82%e0%a6%95%e0%a6%b2%e0%a6%a8-%e0%a6%8f%e0%a6%ac%e0%a6%82-%e0%a6%aa%e0%a6%b0%e0%a6%bf%e0%a6%ae%e0%a6%be%e0%a6%b0%e0%a7%8d%e0%a6%9c/">
@@ -57,7 +57,7 @@ export default class featuredArchive extends Component {
                 <span className="slide__image">
                   <img
                     alt="ইসলামের অন্যতম ভিত্তি তাকদীর প্রসঙ্গে"
-                    src="https://www.nastikya.com/wp-content/uploads/2021/01/takdir_freewill.jpg"
+                    src="https://i0.wp.com/www.shongshoy.com/wp-content/uploads/2021/01/takdir_freewill.jpg"
                   />
                   <div className="text-block">
                     <a href="/%e0%a6%87%e0%a6%b8%e0%a6%b2%e0%a6%be%e0%a6%ae%e0%a7%87%e0%a6%b0-%e0%a6%85%e0%a6%a8%e0%a7%8d%e0%a6%af%e0%a6%a4%e0%a6%ae-%e0%a6%ad%e0%a6%bf%e0%a6%a4%e0%a7%8d%e0%a6%a4%e0%a6%bf-%e0%a6%a4%e0%a6%be/">
@@ -71,7 +71,7 @@ export default class featuredArchive extends Component {
                 <span className="slide__image">
                   <img
                     alt="আয়িশা কি নয়বছর বয়সে বয়ঃসন্ধিকালে পৌঁছেছিলেন?"
-                    src="https://www.nastikya.com/wp-content/uploads/2021/01/stop_child_marriage.jpg"
+                    src="https://i2.wp.com/www.shongshoy.com/wp-content/uploads/2021/01/stop_child_marriage.jpg"
                   />
                   <div className="text-block">
                     <a href="/%e0%a6%86%e0%a7%9f%e0%a6%bf%e0%a6%b6%e0%a6%be-%e0%a6%95%e0%a6%bf-%e0%a6%a8%e0%a7%9f-%e0%a6%ac%e0%a6%9b%e0%a6%b0-%e0%a6%ac%e0%a7%9f%e0%a6%b8%e0%a7%87-%e0%a6%ac%e0%a7%9f%e0%a6%83%e0%a6%b8%e0%a6%a8/">
@@ -85,7 +85,7 @@ export default class featuredArchive extends Component {
                 <span className="slide__image">
                   <img
                     alt="ফাতিমার ঘরে উমর – এক অনন্ত যুদ্ধের সুচনা"
-                    src="https://www.nastikya.com/wp-content/uploads/2021/01/shia_sunni.jpg"
+                    src="https://i2.wp.com/www.shongshoy.com/wp-content/uploads/2021/01/shia_sunni.jpg"
                   />
                   <div className="text-block">
                     <a href="/%e0%a6%ab%e0%a6%be%e0%a6%a4%e0%a6%bf%e0%a6%ae%e0%a6%be%e0%a6%b0-%e0%a6%98%e0%a6%b0%e0%a7%87-%e0%a6%89%e0%a6%ae%e0%a6%b0-%e0%a6%8f%e0%a6%95-%e0%a6%85%e0%a6%a8%e0%a6%a8%e0%a7%8d%e0%a6%a4-%e0%a6%af/">
@@ -99,7 +99,7 @@ export default class featuredArchive extends Component {
                 <span className="slide__image">
                   <img
                     alt="বহুল প্রচলিত কিছু কুযুক্তি বা ফ্যালাসি বা কুতর্ক বা হেত্বাভাস"
-                    src="https://www.nastikya.com/wp-content/uploads/2018/01/logic_shongshoy.jpg"
+                    src="https://i2.wp.com/www.shongshoy.com/wp-content/uploads/2018/01/logic_shongshoy.jpg"
                   />
                   <div className="text-block">
                     <a href="/%e0%a6%95%e0%a7%81%e0%a6%af%e0%a7%81%e0%a6%95%e0%a7%8d%e0%a6%a4%e0%a6%bf-%e0%a6%ac%e0%a6%be-%e0%a6%ab%e0%a7%8d%e0%a6%af%e0%a6%be%e0%a6%b2%e0%a6%be%e0%a6%b8%e0%a6%bf/">
@@ -113,7 +113,7 @@ export default class featuredArchive extends Component {
                 <span className="slide__image">
                   <img
                     alt="নবী মুহাম্মদ কি সর্বকালের সর্বশ্রেষ্ঠ মানুষ ছিলেন?"
-                    src="https://www.nastikya.com/wp-content/uploads/2021/01/Muhammad_warlord.jpg"
+                    src="https://i0.wp.com/www.shongshoy.com/wp-content/uploads/2021/01/Muhammad_warlord.jpg"
                   />
                   <div className="text-block">
                     <a href="/%e0%a6%a8%e0%a6%ac%e0%a7%80-%e0%a6%ae%e0%a7%81%e0%a6%b9%e0%a6%be%e0%a6%ae%e0%a7%8d%e0%a6%ae%e0%a6%a6-%e0%a6%95%e0%a6%bf-%e0%a6%b8%e0%a6%b0%e0%a7%8d%e0%a6%ac%e0%a6%95%e0%a6%be%e0%a6%b2%e0%a7%87/">
